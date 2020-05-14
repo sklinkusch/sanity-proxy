f48eda335789fe65875687f2b2f135bc0b142da5
Stefan Klinkusch <stefan.klinkusch@googlemail.com>
2020-05-14 14:59:57 +0200

    function calls asynchronous

--- index.js
+++ index.js
@@ -7,10 +7,10 @@ const { send } = require("micro")
 module.exports = async (req, res) => {
   const { query } = parse(req.url)
   console.log(`1: ${query}`)
-  const params = parseQueryString(query)
+  const params = await parseQueryString(query)
   console.log(params)
   const { projectId = "8yon6w8q", mode = "production", ...remaining } = params
-  const outputQuery = putTogetherString(remaining)
+  const outputQuery = await putTogetherString(remaining)
   console.log(`2: ${outputQuery}`)
   res.setHeader("Access-Control-Allow-Origin", "*")
   res.setHeader("Content-Type", "application/json")
