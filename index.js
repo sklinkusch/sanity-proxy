ee01a3325dd03c09a0801cef7c509380d655886c
Stefan Klinkusch <stefan.klinkusch@googlemail.com>
2020-05-14 10:49:59 +0200

    query can take projectId and dataset

--- index.js
+++ index.js
@@ -6,10 +6,12 @@ const { send } = require("micro")
 
 module.exports = async (req, res) => {
   const { query } = parse(req.url)
+  const params = parseQueryString(query)
+  const { projectId = "8yon6w8q", mode = "production", ...remaining } = params
+  const outputQuery = putTogetherString(remaining)
   res.setHeader("Access-Control-Allow-Origin", "*")
   res.setHeader("Content-Type", "application/json")
-  const projectId = "8yon6w8q"
-  const requestUrl = `https://${projectId}.api.sanity.io/v1/data/query/production?${query}`
+  const requestUrl = `https://${projectId}.api.sanity.io/v1/data/query/${mode}?${outputQuery}`
   fetch(requestUrl)
     .then((response) => response.json())
     .then((data) => {
@@ -18,4 +20,29 @@ module.exports = async (req, res) => {
     .catch((error) => {
       send(res, 500, error)
     })
+}
+
+const parseQueryString = function (queryString) {
+  const params = {}
+  const queries = queryString.split("&")
+  for (let i = 0; i < queries.length; i++) {
+    const temp = queries[i].split("=")
+    if (temp[0] == "query") {
+      const [queryKey, ...remainder] = temp
+      params[queryKey] = remainder.join("=")
+    } else {
+      const [key, value] = temp
+      params[key] = value
+    }
+  }
+  return params
+}
+
+const putTogetherString = function (queryObject) {
+  const { query, ...otherVals } = queryObject
+  let outputString = `query=${query}`
+  for (let [key, value] of Object.entries(otherVals)) {
+    outputString = `${outputString}&${key}=${value}`
+  }
+  return outputString
 }
